6ad3842518e9b484b77c998633b897e5a3840eb9
David Kamberský <david.kambersky@gmail.com>
2014-08-06 16:49:43 +0200

    Removing broken debug code

--- content.js
+++ content.js
@@ -5,7 +5,6 @@ var pattern3=new RegExp("twitch");
 var pattern4=new RegExp("://extensions");
 var urlA = window.location; 
 
-window.document.getElementById("demo").innerHTML = "Paragraph changed.";
 
 if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA)) // if it matches pattern defined above
 {
