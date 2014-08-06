cd5e441dd8285c859cc465e7716f6da4e027eb9e
David Kamberský <david.kambersky@gmail.com>
2014-08-06 09:58:58 +0200

    Trying to block the Chrome extensions page
    The event doesn't fire :/

--- content.js
+++ content.js
@@ -2,11 +2,12 @@ var pattern=new RegExp("reddit");
 var pattern1=new RegExp("youtube");
 var pattern2=new RegExp("twitter");
 var pattern3=new RegExp("twitch");
+var pattern4=new RegExp("://extensions");
 var urlA = window.location; 
 
+window.document.getElementById("demo").innerHTML = "Paragraph changed.";
 
-
-if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)) // if it matches pattern defined above
+if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA)) // if it matches pattern defined above
 {
   
   chrome.extension.sendRequest({redirect: "http://127.0.0.1:81/v2"}); // send message to redirect
