98c50f5ee0a42c8933ca447b62b5eb26488e7790
David Kamberský <david.kambersky@gmail.com>
2014-08-06 09:36:03 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,14 @@
+var pattern=new RegExp("reddit");
+var pattern1=new RegExp("youtube");
+var pattern2=new RegExp("twitter");
+var pattern3=new RegExp("twitch");
+var urlA = window.location; 
+
+
+
+if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)) // if it matches pattern defined above
+{
+  
+  chrome.extension.sendRequest({redirect: "http://127.0.0.1:81/v2"}); // send message to redirect
+
+} 
