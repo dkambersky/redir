706fc7699825d380c237487f0b0b69a1484a0a32
David Kamberský <david.kambersky@gmail.com>
2014-09-16 23:34:03 +0200

    Personal site list update

--- content.js
+++ content.js
@@ -3,10 +3,11 @@ var pattern1=new RegExp("youtube");
 var pattern2=new RegExp("twitter");
 var pattern3=new RegExp("twitch");
 var pattern4=new RegExp("://extensions");
+var pattern5=new RegExp("9gag");
 var urlA = window.location; 
 
 
-if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA)) // if it matches pattern defined above
+if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA)||pattern5.test(urlA) // if it matches pattern defined above
 {
   
   chrome.extension.sendRequest({redirect: "http://127.0.0.1:81/v2"}); // send message to redirect
