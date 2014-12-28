2a550d22f37c078a4ad5ad4f3e94bf0c5b837ee2
David Kamberský <david.kambersky@gmail.com>
2014-12-28 16:22:51 +0100

    Enable facebook/messages w/o enabling facebook

--- content.js
+++ content.js
@@ -4,11 +4,21 @@ var pattern2=new RegExp("twitter");
 var pattern3=new RegExp("twitch");
 var pattern4=new RegExp("extensions");
 var pattern5=new RegExp("facebook");
+var pattern6=new RegExp("messages"); 
 var urlA = window.location; 
 
+var redir = false; 
 
-if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA) || pattern5.test(urlA) ) // if it matches pattern defined above
+if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA)||pattern4.test(urlA) || pattern5.test(urlA)) // if it matches pattern defined above
 
+{redir = true; }
+
+if (pattern6.test(urlA)){
+redir = false ;}
+
+
+
+if(redir)
 
 {
   
