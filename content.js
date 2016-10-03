4a3cc179997043412a5bef05d52ada8805879208
David Kamberský <david.kambersky@gmail.com>
2016-10-03 13:44:13 +0100

    url

--- content.js
+++ content.js
@@ -1,15 +1,15 @@
 //MANUAL OVERRIDE -- set to 'false' to turn redirecting off
-enabled = true;
+enabled = false;
 
 var pattern = new RegExp("reddit");
 var pattern1 = new RegExp("youtube");
 var pattern2 = new RegExp("twitter");
 var pattern3 = new RegExp("twitch");
 var pattern5 = new RegExp("facebook");
-var pattern6 = new RegExp("messages"); 
-var urlA = window.location; 
+var pattern6 = new RegExp("messages");
+var urlA = window.location;
 
-var redir = false; 
+var redir = false;
 
 if (pattern.test(urlA)||pattern1.test(urlA)||pattern2.test(urlA)||pattern3.test(urlA) || pattern5.test(urlA)) // if it matches pattern defined above
 
@@ -23,7 +23,7 @@ redir = false ;}
 
 
 if(redir && enabled){
-  
+
   chrome.extension.sendRequest({redirect: "chrome://newtab"}); // send message to redirect
 
-} 
+}
