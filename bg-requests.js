cbaeca3fc76393758c93acb027a2b72f20a77eb4
David Kamberský <david.kambersky@gmail.com>
2016-11-21 15:22:15 +0000

    Storage's broken yo

--- bg-requests.js
+++ bg-requests.js
@@ -3,7 +3,11 @@
 //TODO dynamic permission allocation
 
 var enabled = true;
+var inited = false;
 var redirURL = "https://google.co.uk";
+var shit = 5;
+var waiting = 0;
+
 
 var sites = [
     "reddit",
@@ -29,13 +33,11 @@ chrome.webRequest.onBeforeRequest.addListener(
 
     }, {
         urls: [
-          // This function only gets called on URLs we have permissions for anyway
+            // This function only gets called on URLs we have permissions for anyway
             "*://*/*"
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     }, ["blocking"]);
-
-alert('running');
 
 function match(string) {
     var redir = false;
@@ -56,3 +58,89 @@ function match(string) {
 
     return redir;
 }
+
+
+// / Config helper functions
+
+// Initial setup
+function initSettings() {
+    loadSettings(1);
+
+    if (!inited) {
+        saveSetting('mainUrl', 'https://dkambersky.io/v2');
+      saveSetting('inited', 'true');
+        saveSetting('enabled', 'true');
+/*        saveSetting('sites', [
+            "reddit",
+            "youtube",
+            "twitter",
+            "twitch",
+            "facebook"
+        ]);
+
+        saveSetting('allowed', [
+            "facebook.com/messagesxx"
+        ]);
+        saveSetting('shit', '10');*/
+
+        console.log('init settings should be done');
+
+        loadSettings(true);
+    }
+}
+//clearSettings();
+inited = false;
+initSettings();
+alert(shit);
+
+
+// Reload config when stored config changes
+chrome.storage.onChanged.addListener(function(changes, area) {
+    if (area === "sync") {
+        loadSettings();
+    }
+});
+
+function loadSettings(f) {
+    chrome.storage.sync.get('shit', function(items) {
+        if (items.inited === 'true' || f) {
+          console.log('Loading. Waiting:' + waiting );
+            for (key in items) {
+                console.log('for: ' + key + '  |  ' + items[key]);
+            }
+
+            inited = true;
+            redirURL = items.mainUrl;
+            enabled = items.enabled;
+            sites = items.sites;
+            allowed = items.allowed;
+            shit = 10;
+            console.log(shit + ' should be 10, stored: ' + items.shit + items.mainUrl);
+
+        }
+    });
+}
+
+
+function saveSetting(key, value) {
+    waiting++;
+    console.log('Saving a value: ' + key + '. Waiting, this including: ' + waiting);
+    chrome.storage.sync.set({
+        key: value
+    }, function() {
+        console.log('value ' + value + ' set for key ' + key);
+        waiting--;
+    });
+    if (chrome.runtime.lastError != null) {
+        alert(chrome.runtime.lastError.message + ': error');
+    }
+}
+
+function clearSettings() {
+  console.log('clearingSettings');
+    chrome.storage.sync.clear();
+}
+
+chrome.storage.local.get(function(result) {
+    console.log(result)
+})
